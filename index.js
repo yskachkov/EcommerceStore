1f1e05cf7e08f9ce28ddd88d5ea5c3dd242ce787
Yevhenii Skachkov <yevhenii_skachkov@epam.com>
2020-03-18 20:16:23 +0200

    Add reactotron

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ import { AppRegistry } from 'react-native';
 
 import App from './src/App';
 import { name as appName } from './app.json';
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+}
+
+// reactotron demo
+Reactotron.display({
+  name: 'React Native',
+  preview: 'Hello',
+  value: 'world',
+  important: true
+});
 
 AppRegistry.registerComponent(appName, () => App);
